ff3aa88accc2beeb5cbe58f05f59148f186e87ce
p1nu <pichcrazy11@gmail.com>
2024-11-12 11:12:08 +0700

    update add jobs design and connected to backend

--- src/scenes/jobs/AddJob.jsx
+++ src/scenes/jobs/AddJob.jsx
@@ -1,54 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Button, InputBase, Typography, useTheme, MenuItem, Select, FormControl, TextField } from '@mui/material';
-import axios from 'axios';
-import { tokens } from '../../theme';
-import Header from '../../components/Header';
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+  InputBase,
+} from "@mui/material";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+import axios from "axios";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+import { useNavigate } from "react-router-dom";
 
 const AddJob = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [job, setJob] = useState({
-    job_title: '',
-    job_description: '',
-    job_requirements: '',
-    job_status_id: '',
+    job_name: "",
+    job_desc: "",
+    job_salary: "",
+    job_schedule: "",
+    job_link: "",
+    job_start_date: "",
+    job_end_date: "",
+    job_company_id: "",
+    job_created_by_user_id: "",
   });
-  const [error, setError] = useState('');
+  const [companies, setCompanies] = useState([]);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setJob((prevJob) => ({ ...prevJob, [name]: value }));
   };
 
+  const handleDescriptionChange = (value) => {
+    setJob((prevJob) => ({ ...prevJob, job_desc: value }));
+  };
+
   const handleAddJob = async () => {
     try {
-      await axios.post('http://localhost:3030/job', job);
-      setError('Job added successfully');
+      await axios.post("http://localhost:3030/job/new", job);
+      setError("Job added successfully");
       setJob({
-        job_title: '',
-        job_description: '',
-        job_requirements: '',
-        job_status_id: '',
-      }); // Reset form
+        job_name: "",
+        job_desc: "",
+        job_salary: "",
+        job_schedule: "",
+        job_link: "",
+        job_start_date: "",
+        job_end_date: "",
+        job_company_id: "",
+        job_created_by_user_id: "",
+      });
+      setTimeout(() => {
+        navigate("/jobs");
+      }, 3000);
     } catch (error) {
-      console.error('Error adding job:', error);
-      setError('Error adding job');
+      console.error("Error adding job:", error);
+      setError("Error adding job");
     }
   };
 
   useEffect(() => {
+    // Fetch company data
+    const fetchCompanies = async () => {
+      try {
+        const response = await axios.get("http://localhost:3030/company/all");
+        setCompanies(response.data);
+      } catch (error) {
+        console.error("Error fetching companies:", error);
+      }
+    };
+    fetchCompanies();
+  }, []);
+
+  useEffect(() => {
     if (error) {
       const timer = setTimeout(() => {
-        setError('');
-      }, 3000); // Clear error after 3 seconds
+        setError("");
+      }, 3000);
 
-      return () => clearTimeout(timer); // Cleanup the timer on component unmount
+      return () => clearTimeout(timer);
     }
   }, [error]);
 
   return (
     <Box m={2}>
-      <Header title="Add New Job" subTitle="Create a new job" />
+      <Header title="Add New Job" subTitle="Create a new job posting" />
       <Box
         display="flex"
         flexDirection="column"
@@ -57,6 +102,16 @@ const AddJob = () => {
         height="100%"
         padding={2}
         bgcolor={colors.grey[800]}
+        sx={{
+          "& .ql-container.ql-snow": {
+            width: "100% !important",
+            height: "84% !important",
+            border: "1px solid #000",
+          },
+          "& .ql-toolbar": {
+            border: "1px solid #000",
+          },
+        }}
       >
         <Box
           display="flex"
@@ -66,98 +121,279 @@ const AddJob = () => {
           p={4}
           bgcolor={colors.grey[900]}
           borderRadius="2px"
-          width="50%" 
+          width="100%"
           boxShadow={3}
         >
-          <Typography variant="h4" color={colors.grey[100]} mb={2}>
-            Add Job
-          </Typography>
-          <InputBase
-            placeholder="Job Title"
-            name="job_title"
-            value={job.job_title}
-            onChange={handleChange}
-            sx={{
-              width: '100%',
-              margin: '10px 0',
-              padding: '10px',
-              border: `1px solid ${colors.grey[800]}`,
-              borderRadius: '2px',
-              backgroundColor: colors.grey[900],
-              color: colors.grey[100],
-            }}
-          />
-          <TextField
-            placeholder="Job Description"
-            name="job_description"
-            value={job.job_description}
-            onChange={handleChange}
-            multiline
-            rows={4}
-            variant="outlined"
-            InputProps={{
-              style: {
-                color: colors.grey[100],
-                backgroundColor: colors.grey[900],
-                borderRadius: '2px',
-                padding: '10px',
-              },
-            }}
-            InputLabelProps={{
-              style: {
-                color: colors.grey[100],
-              },
-            }}
-            sx={{
-              width: '100%',
-              margin: '10px 0',
-              border: `1px solid ${colors.grey[800]}`,
-            }}
-          />
-          <TextField
-            placeholder="Job Requirements"
-            name="job_requirements"
-            value={job.job_requirements}
-            onChange={handleChange}
-            multiline
-            rows={4}
-            variant="outlined"
-            InputProps={{
-              style: {
-                color: colors.grey[100],
-                backgroundColor: colors.grey[900],
-                borderRadius: '2px',
-                padding: '10px',
-              },
-            }}
-            InputLabelProps={{
-              style: {
-                color: colors.grey[100],
-              },
-            }}
-            sx={{
-              width: '100%',
-              margin: '10px 0',
-              border: `1px solid ${colors.grey[800]}`,
-            }}
-          />
-          <FormControl fullWidth sx={{ margin: '10px 0' }}>
-            <Select
-              name="job_status_id"
-              value={job.job_status_id}
+          {/* Job Name */}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            gap="20px"
+            width="100%"
+            alignItems={"center"}
+          >
+            <Box display="flex" flexDirection="column" width="100%">
+              <InputLabel
+                htmlFor="job_name"
+                sx={{ color: colors.grey[100], mb: "5px" }}
+              >
+                Job Name
+              </InputLabel>
+              <InputBase
+                id="job_name"
+                placeholder="Job Name"
+                name="job_name"
+                value={job.job_name}
+                onChange={handleChange}
+                sx={{
+                  padding: "10px",
+                  border: `1px solid #000`,
+                  borderRadius: "2px",
+                  backgroundColor: colors.grey[900],
+                  color: colors.grey[100],
+                }}
+              />
+            </Box>
+            {/* Company Selection */}
+            <Box display="flex" flexDirection="column" width="100%">
+              <InputLabel
+                htmlFor="job_company_id"
+                sx={{ color: colors.grey[100], mb: "5px" }}
+              >
+                Company
+              </InputLabel>
+              <FormControl fullWidth>
+                <Select
+                  id="job_company_id"
+                  name="job_company_id"
+                  value={job.job_company_id}
+                  onChange={handleChange}
+                  displayEmpty
+                  sx={{
+                    border: `1px solid #000`,
+                    borderRadius: "2px",
+                    backgroundColor: colors.grey[900],
+                    color: colors.grey[100],
+                  }}
+                >
+                  <MenuItem value="" disabled>
+                    Select Company
+                  </MenuItem>
+                  {companies.map((company) => (
+                    <MenuItem
+                      key={company.company_id}
+                      value={company.company_id}
+                    >
+                      {company.company_name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+          {/* Job Link */}
+          <Box
+            display="flex"
+            flexDirection="column"
+            margin="10px 0"
+            width="100%"
+          >
+            <InputLabel
+              htmlFor="job_link"
+              sx={{ color: colors.grey[100], mb: "5px" }}
+            >
+              Job Link
+            </InputLabel>
+            <InputBase
+              id="job_link"
+              placeholder="Job Link"
+              name="job_link"
+              value={job.job_link}
               onChange={handleChange}
               sx={{
-                border: `1px solid ${colors.grey[800]}`,
-                borderRadius: '2px',
+                padding: "10px",
+                border: `1px solid #000`,
+                borderRadius: "2px",
                 backgroundColor: colors.grey[900],
                 color: colors.grey[100],
-                marginTop: '10px',
               }}
+            />
+          </Box>
+
+          {/* Job Description */}
+          <Box
+            display="flex"
+            flexDirection="column"
+            margin="10px 0"
+            width="100%"
+          >
+            <InputLabel
+              htmlFor="job_desc"
+              sx={{ color: colors.grey[100], mb: "5px" }}
             >
-              <MenuItem value="1">Open</MenuItem>
-              <MenuItem value="2">Closed</MenuItem>
-            </Select>
-          </FormControl>
+              Job Description
+            </InputLabel>
+            <ReactQuill
+              theme="snow"
+              value={job.job_desc}
+              onChange={handleDescriptionChange}
+              style={{
+                height: "200px",
+                backgroundColor: colors.grey[900],
+                color: colors.grey[100],
+              }}
+            />
+          </Box>
+
+          {/* Job Salary and Job Schedule on the Same Line */}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            gap="20px"
+            margin="10px 0"
+            width="100%"
+          >
+            {/* Job Salary */}
+            <Box display="flex" flexDirection="column" flex={1}>
+              <InputLabel
+                htmlFor="job_salary"
+                sx={{ color: colors.grey[100], mb: "5px" }}
+              >
+                Job Salary
+              </InputLabel>
+              <InputBase
+                id="job_salary"
+                placeholder="Job Salary"
+                name="job_salary"
+                type="number"
+                value={job.job_salary}
+                onChange={handleChange}
+                sx={{
+                  padding: "10px",
+                  border: `1px solid #000`,
+                  borderRadius: "2px",
+                  backgroundColor: colors.grey[900],
+                  color: colors.grey[100],
+                }}
+              />
+            </Box>
+
+            {/* Job Schedule */}
+            <Box display="flex" flexDirection="column" flex={1}>
+              <InputLabel
+                htmlFor="job_schedule"
+                sx={{ color: colors.grey[100], mb: "5px" }}
+              >
+                Job Schedule
+              </InputLabel>
+              <InputBase
+                id="job_schedule"
+                placeholder="Job Schedule"
+                name="job_schedule"
+                type="text"
+                value={job.job_schedule}
+                onChange={handleChange}
+                sx={{
+                  padding: "10px",
+                  border: `1px solid #000`,
+                  borderRadius: "2px",
+                  backgroundColor: colors.grey[900],
+                  color: colors.grey[100],
+                }}
+              />
+            </Box>
+          </Box>
+
+          {/* Job Start Date and Job End Date on the Same Line */}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            gap="20px"
+            margin="10px 0"
+            width="100%"
+          >
+            {/* Job Start Date */}
+            <Box display="flex" flexDirection="column" flex={1}>
+              <InputLabel
+                htmlFor="job_start_date"
+                sx={{ color: colors.grey[100], mb: "5px" }}
+              >
+                Job Start Date
+              </InputLabel>
+              <InputBase
+                id="job_start_date"
+                placeholder="Job Start Date"
+                name="job_start_date"
+                type="date"
+                value={job.job_start_date}
+                onChange={handleChange}
+                sx={{
+                  padding: "10px",
+                  border: `1px solid #000`,
+                  borderRadius: "2px",
+                  backgroundColor: colors.grey[900],
+                  color: colors.grey[100],
+                }}
+              />
+            </Box>
+
+            {/* Job End Date */}
+            <Box display="flex" flexDirection="column" flex={1}>
+              <InputLabel
+                htmlFor="job_end_date"
+                sx={{ color: colors.grey[100], mb: "5px" }}
+              >
+                Job End Date
+              </InputLabel>
+              <InputBase
+                id="job_end_date"
+                placeholder="Job End Date"
+                name="job_end_date"
+                type="date"
+                value={job.job_end_date}
+                onChange={handleChange}
+                fullWidth
+                sx={{
+                  padding: "10px",
+                  border: `1px solid #000`,
+                  borderRadius: "2px",
+                  backgroundColor: colors.grey[900],
+                  color: colors.grey[100],
+                }}
+              />
+            </Box>
+          </Box>
+
+          {/* Created By User ID */}
+          <Box
+            display="flex"
+            flexDirection="column"
+            margin="10px 0"
+            width="100%"
+          >
+            <InputLabel
+              htmlFor="job_created_by_user_id"
+              sx={{ color: colors.grey[100], mb: "5px" }}
+            >
+              Created By User ID
+            </InputLabel>
+            <InputBase
+              id="job_created_by_user_id"
+              placeholder="Created By User ID"
+              name="job_created_by_user_id"
+              value={job.job_created_by_user_id}
+              onChange={handleChange}
+              sx={{
+                padding: "10px",
+                border: `1px solid #000`,
+                borderRadius: "2px",
+                backgroundColor: colors.grey[900],
+                color: colors.grey[100],
+              }}
+            />
+          </Box>
+
+          {/* Submit Button */}
           <Button
             variant="contained"
             fullWidth
@@ -166,8 +402,14 @@ const AddJob = () => {
           >
             Add Job
           </Button>
+
+          {/* Error/Success Message */}
           {error && (
-            <Typography variant="body1" color={error.includes('successfully') ? 'green' : 'red'} mt={2}>
+            <Typography
+              variant="body1"
+              color={error.includes("successfully") ? "green" : "red"}
+              mt={2}
+            >
               {error}
             </Typography>
           )}
