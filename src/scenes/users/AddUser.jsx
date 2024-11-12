315ab0794472029eef81cd85c22bed39969ca945
p1nu <pichcrazy11@gmail.com>
2024-11-12 21:07:36 +0700

    update add user form design

--- src/scenes/users/AddUser.jsx
+++ src/scenes/users/AddUser.jsx
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Button, InputBase, Typography, useTheme, MenuItem, Select, FormControl } from '@mui/material';
-import axios from 'axios';
-import { tokens } from '../../theme';
-import Header from '../../components/Header';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Button,
+  InputBase,
+  Typography,
+  useTheme,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import axios from "axios";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+import { useNavigate } from "react-router-dom";
 
 const AddUser = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const [user, setUser] = useState({ user_name: '', user_password: '', user_role_id: '' });
-  const [error, setError] = useState('');
+  const [user, setUser] = useState({
+    user_name: "",
+    user_password: "",
+    user_role_id: "",
+  });
+  const [error, setError] = useState("");
 
   const navegate = useNavigate();
 
@@ -20,22 +34,22 @@ const AddUser = () => {
 
   const handleAddUser = async () => {
     try {
-      await axios.post('http://localhost:3030/user/new', user);
-      setError('User added successfully');
-      setUser({ user_name: '', user_password: '', user_role_id: '' }); // Reset form
+      await axios.post("http://localhost:3030/user/new", user);
+      setError("User added successfully");
+      setUser({ user_name: "", user_password: "", user_role_id: "" }); // Reset form
       setTimeout(() => {
-        navegate('/users');
+        navegate("/users");
       }, 3000); // Navigate to users page after 3 seconds
     } catch (error) {
-      console.error('Error adding user:', error);
-      setError('Error adding user');
+      console.error("Error adding user:", error);
+      setError("Error adding user");
     }
   };
 
   useEffect(() => {
     if (error) {
       const timer = setTimeout(() => {
-        setError('');
+        setError("");
       }, 3000); // Clear error after 3 seconds
 
       return () => clearTimeout(timer); // Cleanup the timer on component unmount
@@ -64,58 +78,81 @@ const AddUser = () => {
           boxShadow={3}
           width="50%"
         >
-          <InputBase
-            placeholder="Username"
-            name="user_name"
-            value={user.user_name}
-            onChange={handleChange}
-            sx={{
-              width: '100%',
-              margin: '10px 0',
-              padding: '10px',
-              border: `1px solid #000`,
-              borderRadius: '2px',
-              backgroundColor: colors.grey[900],
-              color: colors.grey[100],
-            }}
-          />
-          <InputBase
-            placeholder="Password"
-            name="user_password"
-            type="password"
-            value={user.user_password}
-            onChange={handleChange}
-            sx={{
-              width: '100%',
-              margin: '10px 0',
-              padding: '10px',
-              border: `1px solid #000`,
-              borderRadius: '2px',
-              backgroundColor: colors.grey[900],
-              color: colors.grey[100],
-            }}
-          />
-          <FormControl fullWidth sx={{ margin: '10px 0' }}>
-            <Select
-              placeholder='User Role'
-              name="user_role_id"
-              value={user.user_role_id}
+          <Box display="flex" flexDirection="column" width="100%">
+            <InputLabel
+              htmlFor="user_name"
+              sx={{ color: colors.grey[100], mb: "5px" }}
+            >
+              Company Name
+            </InputLabel>
+            <InputBase
+              id="user_name"
+              placeholder="Enter User name"
+              name="user_name"
+              value={user.user_name}
               onChange={handleChange}
-              displayEmpty
               sx={{
+                padding: "10px",
                 border: `1px solid #000`,
-                borderRadius: '2px',
+                borderRadius: "2px",
                 backgroundColor: colors.grey[900],
                 color: colors.grey[100],
-                marginTop: '10px',
+                mb: "10px",
               }}
-            > 
-              <MenuItem value="" disabled>User Role</MenuItem>
-              <MenuItem value="1">Admin</MenuItem>
-              <MenuItem value="2">User</MenuItem>
-              {/* <MenuItem value="3">Guest</MenuItem> */}
-            </Select>
-          </FormControl>
+            />
+          </Box>
+          <Box display="flex" flexDirection="column" width="100%">
+            <InputLabel
+              htmlFor="user_password"
+              sx={{ color: colors.grey[100], mb: "5px" }}
+            >
+              Company Name
+            </InputLabel>
+            <InputBase
+              id="user_password"
+              placeholder="Enter User Password"
+              name="user_password"
+              value={user.user_password}
+              onChange={handleChange}
+              sx={{
+                padding: "10px",
+                border: `1px solid #000`,
+                borderRadius: "2px",
+                backgroundColor: colors.grey[900],
+                color: colors.grey[100],
+                mb: "10px",
+              }}
+            />
+          </Box>
+          <Box display="flex" flexDirection="column" width="100%">
+            <InputLabel
+              htmlFor="user_role_id"
+              sx={{ color: colors.grey[100], mb: "5px" }}
+            >
+              Company
+            </InputLabel>
+            <FormControl fullWidth>
+              <Select
+                id="user_role_id"
+                name="user_role_id"
+                value={user.user_role_id}
+                onChange={handleChange}
+                displayEmpty
+                sx={{
+                  border: `1px solid #000`,
+                  borderRadius: "2px",
+                  backgroundColor: colors.grey[900],
+                  color: colors.grey[100],
+                }}
+              >
+                <MenuItem value="" disabled>
+                  Select User Role
+                </MenuItem>
+                <MenuItem value="1">Admin</MenuItem>
+                <MenuItem value="2">User</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
           <Button
             variant="contained"
             fullWidth
@@ -125,7 +162,11 @@ const AddUser = () => {
             Add User
           </Button>
           {error && (
-            <Typography variant="body1" color={error.includes('successfully') ? 'green' : 'red'} mt={2}>
+            <Typography
+              variant="body1"
+              color={error.includes("successfully") ? "green" : "red"}
+              mt={2}
+            >
               {error}
             </Typography>
           )}
