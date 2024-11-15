211cea5b46e7ac9da95fd786c66b28285f8a53eb
p1nu <pichcrazy11@gmail.com>
2024-11-15 16:07:56 +0700

    fix typo in authcontext

--- src/scenes/global/AuthContext.jsx
+++ src/scenes/global/AuthContext.jsx
@@ -30,9 +30,9 @@ export const AuthProvider = ({ children }) => {
       setIsAuthenticated(true);
       setUser({
         user_token: token,
-        user_name: decodedToken.uesr_name || decodedToken.userName || 'User',
+        user_name: decodedToken.user_name || decodedToken.userName || 'User', // Fixed typo
         user_role_id: decodedToken.user_role_id || 2, // Adjust based on your token's payload
-        user_id: decodedToken.user_id || '',// Add other relevant user information here
+        user_id: decodedToken.user_id || '', // Add other relevant user information here
       });
 
       // Optionally, navigate to a protected route after login
@@ -77,9 +77,9 @@ export const AuthProvider = ({ children }) => {
           setIsAuthenticated(true);
           setUser({
             user_token: token,
-            user_name: decodedToken.user_name || decodedToken.userName || 'User',
+            user_name: decodedToken.user_name || decodedToken.userName || 'User', // Fixed typo
             user_role_id: decodedToken.user_role_id || 2, // Adjust based on your token's payload
-            user_id: decodedToken.user_id || ''// Add other relevant user information here
+            user_id: decodedToken.user_id || '', // Add other relevant user information here
           });
         }
       } catch (error) {
