718e7e97bdf761fd8fea569f551c1640f434271c
p1nu <pichcrazy11@gmail.com>
2024-11-21 12:54:35 +0700

    make csr funcitonable

--- src/scenes/csr/AddCSR.jsx
+++ src/scenes/csr/AddCSR.jsx
@@ -1,14 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Button, Typography, useTheme, InputLabel } from '@mui/material';
-import InputBase from '@mui/material/InputBase';
-import axios from 'axios';
-import { tokens } from '../../theme';
-import Header from '../../components/Header';
-import { useNavigate } from 'react-router-dom';
-import ReactQuill from 'react-quill-new';
-import 'react-quill-new/dist/quill.snow.css';
+import React, { useState, useContext, useEffect } from "react";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  InputLabel,
+  Modal,
+  FormControl,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import InputBase from "@mui/material/InputBase";
+import axios from "axios";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+import { useNavigate } from "react-router-dom";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+import { AuthContext } from "../global/AuthContext";
+import { MediaLibrary } from "../gallery/Index";
+import { useMediaGallery } from "../gallery/MediaGalleryContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AddCSR = () => {
+  const { user } = useContext(AuthContext);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const navigate = useNavigate();
@@ -19,15 +35,28 @@ const AddCSR = () => {
     csr_date: "",
     csr_image_id: "",
     csr_link: "",
-    csr_created_by_user_id: "",
+    csr_status_id: 1,
+    csr_created_by_user_id: user?.user_id || "",
   });
-  const [error, setError] = useState('');
 
+  const [image, setImage] = useState("");
+  const { open, handleOpen, handleClose } = useMediaGallery();
+
+  // Handle selecting an image from the media gallery
+  const handleSelectImage = (image) => {
+    setCsr((prevCsr) => ({ ...prevCsr, csr_image_id: image.il_id }));
+    setImage(image.il_path);
+    toast.success("Image selected successfully");
+    handleClose();
+  };
+
+  // Handle form input changes
   const handleChange = (e) => {
     const { name, value } = e.target;
     setCsr((prevCsr) => ({ ...prevCsr, [name]: value }));
   };
 
+  // Handle article changes with react-quill
   const handleArticleChange = (value) => {
     setCsr((prevCsr) => ({ ...prevCsr, csr_article: value }));
   };
@@ -35,33 +64,33 @@ const AddCSR = () => {
   const handleAddCsr = async () => {
     try {
       await axios.post("http://localhost:3030/csr/new", csr);
-      setError("CSR added successfully");
+      toast.success("CSR added successfully");
       setCsr({
         csr_title: "",
         csr_article: "",
         csr_date: "",
         csr_image_id: "",
         csr_link: "",
-        csr_created_by_user_id: "",
+        csr_status_id: 1,
+        csr_created_by_user_id: user?.user_id || "",
       }); // Reset form
       setTimeout(() => {
         navigate("/csr");
       }, 3000);
     } catch (error) {
-      console.error("Error adding CSR:", error);
-      setError("Error adding CSR");
+      console.error(error);
+      toast.error(error.response.data.message);
     }
   };
 
   useEffect(() => {
-    if (error) {
-      const timer = setTimeout(() => {
-        setError('');
-      }, 3000); // Clear error after 3 seconds
-
-      return () => clearTimeout(timer); // Cleanup the timer on component unmount
+    if (user) {
+      setCsr((prevCsr) => ({
+        ...prevCsr,
+        csr_created_by_user_id: user.user_id,
+      }));
     }
-  }, [error]);
+  }, [user]);
 
   return (
     <Box m={2}>
@@ -95,177 +124,196 @@ const AddCSR = () => {
           width="100%"
           boxShadow={3}
         >
-          {/* CSR Title */}
           <Box
-            display="flex"
-            flexDirection="column"
-            margin="10px 0"
-            width="100%"
+            display={"flex"}
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            width={"100%"}
+            gap={2}
           >
-            <InputLabel
-              htmlFor="csr_title"
-              sx={{ color: colors.grey[100], mb: '5px' }}
-            >
-              CSR Title
-            </InputLabel>
-            <InputBase
-              id="csr_title"
-              placeholder="CSR Title"
-              name="csr_title"
-              value={csr.csr_title}
-              onChange={handleChange}
-              sx={{
-                padding: '10px',
-                border: '1px solid #000',
-                borderRadius: '2px',
-                backgroundColor: colors.grey[900],
-                color: colors.grey[100],
-              }}
-            />
-          </Box>
+            <Box width={"55%"}>
+              {/* CSR Title */}
+              <Box
+                display="flex"
+                flexDirection="column"
+                margin="10px 0"
+                width="100%"
+              >
+                <InputLabel
+                  htmlFor="csr_title"
+                  sx={{ color: colors.grey[100], mb: "5px" }}
+                >
+                  CSR Title
+                </InputLabel>
+                <InputBase
+                  id="csr_title"
+                  placeholder="CSR Title"
+                  name="csr_title"
+                  value={csr.csr_title}
+                  onChange={handleChange}
+                  sx={{
+                    padding: "10px",
+                    border: `1px solid #000`,
+                    borderRadius: "2px",
+                    backgroundColor: colors.grey[900],
+                    color: colors.grey[100],
+                  }}
+                />
+              </Box>
 
-          {/* CSR Article */}
-          <Box
-            display="flex"
-            flexDirection="column"
-            margin="10px 0"
-            width="100%"
-          >
-            <InputLabel
-              htmlFor="csr_article"
-              sx={{ color: colors.grey[100], mb: '5px' }}
-            >
-              CSR Article
-            </InputLabel>
-            <ReactQuill
-              theme="snow"
-              value={csr.csr_article}
-              onChange={handleArticleChange}
-              placeholder="Write your CSR article here..."
-              style={{
-                backgroundColor: colors.grey[900],
-                color: colors.grey[100],
-                borderRadius: "2px",
-              }}
-            />
-          </Box>
+              {/* CSR Article */}
+              <Box
+                display="flex"
+                flexDirection="column"
+                margin="10px 0"
+                width="100%"
+              >
+                <InputLabel
+                  htmlFor="csr_article"
+                  sx={{ color: colors.grey[100], mb: "5px" }}
+                >
+                  CSR Article
+                </InputLabel>
+                <ReactQuill
+                  theme="snow"
+                  value={csr.csr_article}
+                  onChange={handleArticleChange}
+                  placeholder="Write your article here..."
+                  style={{
+                    backgroundColor: colors.grey[900],
+                    color: colors.grey[100],
+                    borderRadius: "2px",
+                  }}
+                />
+              </Box>
 
-          {/* CSR Date */}
-          <Box
-            display="flex"
-            flexDirection="column"
-            margin="10px 0"
-            width="100%"
-          >
-            <InputLabel
-              htmlFor="csr_date"
-              sx={{ color: colors.grey[100], mb: '5px' }}
-            >
-              CSR Date
-            </InputLabel>
-            <InputBase
-              id="csr_date"
-              name="csr_date"
-              type="date"
-              value={csr.csr_date}
-              onChange={handleChange}
-              sx={{
-                padding: '10px',
-                border: '1px solid #000',
-                borderRadius: '2px',
-                backgroundColor: colors.grey[900],
-                color: colors.grey[100],
-              }}
-            />
-          </Box>
+              {/* CSR Date */}
+              <Box
+                display="flex"
+                flexDirection="column"
+                margin="10px 0"
+                width="100%"
+              >
+                <InputLabel
+                  htmlFor="csr_date"
+                  sx={{ color: colors.grey[100], mb: "5px" }}
+                >
+                  CSR Date
+                </InputLabel>
+                <InputBase
+                  id="csr_date"
+                  name="csr_date"
+                  type="date"
+                  value={csr.csr_date}
+                  onChange={handleChange}
+                  sx={{
+                    padding: "10px",
+                    border: `1px solid #000`,
+                    borderRadius: "2px",
+                    backgroundColor: colors.grey[900],
+                    color: colors.grey[100],
+                  }}
+                />
+              </Box>
 
-          {/* CSR Image ID */}
-          <Box
-            display="flex"
-            flexDirection="column"
-            margin="10px 0"
-            width="100%"
-          >
-            <InputLabel
-              htmlFor="csr_image_id"
-              sx={{ color: colors.grey[100], mb: '5px' }}
-            >
-              CSR Image ID
-            </InputLabel>
-            <InputBase
-              id="csr_image_id"
-              placeholder="Image ID"
-              name="csr_image_id"
-              value={csr.csr_image_id}
-              onChange={handleChange}
-              sx={{
-                padding: '10px',
-                border: '1px solid #000',
-                borderRadius: '2px',
-                backgroundColor: colors.grey[900],
-                color: colors.grey[100],
-              }}
-            />
-          </Box>
+              {/* CSR Link */}
+              <Box
+                display="flex"
+                flexDirection="column"
+                margin="10px 0"
+                width="100%"
+              >
+                <InputLabel
+                  htmlFor="csr_link"
+                  sx={{ color: colors.grey[100], mb: "5px" }}
+                >
+                  CSR Link
+                </InputLabel>
+                <InputBase
+                  id="csr_link"
+                  placeholder="https://example.com"
+                  name="csr_link"
+                  value={csr.csr_link}
+                  onChange={handleChange}
+                  sx={{
+                    padding: "10px",
+                    border: `1px solid #000`,
+                    borderRadius: "2px",
+                    backgroundColor: colors.grey[900],
+                    color: colors.grey[100],
+                  }}
+                />
+              </Box>
 
-          {/* CSR Link */}
-          <Box
-            display="flex"
-            flexDirection="column"
-            margin="10px 0"
-            width="100%"
-          >
-            <InputLabel
-              htmlFor="csr_link"
-              sx={{ color: colors.grey[100], mb: '5px' }}
+              {/* CSR status */}
+              <Box
+                display="flex"
+                flexDirection="column"
+                margin="10px 0"
+                width="100%"
+              >
+                <InputLabel
+                  htmlFor="csr_status_id"
+                  sx={{ color: colors.grey[100], mb: "5px" }}
+                >
+                  Company Status
+                </InputLabel>
+                <FormControl fullWidth>
+                  <Select
+                    id="csr_status_id"
+                    name="csr_status_id"
+                    value={csr.csr_status_id}
+                    onChange={handleChange}
+                    displayEmpty
+                    sx={{
+                      border: `1px solid #000`,
+                      borderRadius: "2px",
+                      backgroundColor: colors.grey[900],
+                      color: colors.grey[100],
+                      "& :hover": {
+                        border: "none !important",
+                      },
+                      "& :focus": {
+                        border: "none",
+                      },
+                      "& .active": {
+                        border: "none",
+                      },
+                    }}
+                  >
+                    <MenuItem value="" disabled>
+                      Select Status
+                    </MenuItem>
+                    <MenuItem value="1">Active</MenuItem>
+                    <MenuItem value="2">Inactive</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </Box>
+            <Box
+              width="45%"
+              border={"1px solid #000"}
+              display={"flex"}
+              justifyContent={"center"}
+              textAlign={"center"}
+              alignItems={"center"}
+              onClick={handleOpen}
             >
-              CSR Link
-            </InputLabel>
-            <InputBase
-              id="csr_link"
-              placeholder="https://example.com"
-              name="csr_link"
-              value={csr.csr_link}
-              onChange={handleChange}
-              sx={{
-                padding: '10px',
-                border: '1px solid #000',
-                borderRadius: '2px',
-                backgroundColor: colors.grey[900],
-                color: colors.grey[100],
-              }}
-            />
+              {image ? (
+                <img
+                  src={`http://localhost:3030/uploads/${image}`}
+                  alt="csr"
+                  width="100%"
+                  height="auto"
+                />
+              ) : (
+                <Typography variant="h6">Click here to select image</Typography>
+              )}
+            </Box>
           </Box>
-
-          {/* Created By User ID */}
-          <Box
-            display="flex"
-            flexDirection="column"
-            margin="10px 0"
-            width="100%"
-          >
-            <InputLabel
-              htmlFor="csr_created_by_user_id"
-              sx={{ color: colors.grey[100], mb: '5px' }}
-            >
-              Created By User ID
-            </InputLabel>
-            <InputBase
-              id="csr_created_by_user_id"
-              placeholder="User ID"
-              name="csr_created_by_user_id"
-              value={csr.csr_created_by_user_id}
-              onChange={handleChange}
-              sx={{
-                padding: '10px',
-                border: '1px solid #000',
-                borderRadius: '2px',
-                backgroundColor: colors.grey[900],
-                color: colors.grey[100],
-              }}
-            />
-          </Box>
-
+          <Modal open={open} onClose={handleClose}>
+            <MediaLibrary onSelectImage={handleSelectImage} />
+          </Modal>
           {/* Submit Button */}
           <Button
             variant="contained"
@@ -275,19 +323,9 @@ const AddCSR = () => {
           >
             Add CSR
           </Button>
-
-          {/* Error/Success Message */}
-          {error && (
-            <Typography
-              variant="body1"
-              color={error.includes('successfully') ? 'green' : 'red'}
-              mt={2}
-            >
-              {error}
-            </Typography>
-          )}
         </Box>
       </Box>
+      <ToastContainer theme="colored" autoClose={2000} />
     </Box>
   );
 };
